62dfaa42c24b4683eb8ab262f90b452e06c1169c
Schenz <bschenz@gmail.com>
2018-08-08 16:01:44 -0400

    final solution

--- RnaTranscription.cs
+++ RnaTranscription.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 public static class RnaTranscription
 {
+
     public static string ToRna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var map = new Dictionary<string, string>() { {"A", "U"}, {"C", "G"}, {"G", "C"}, {"T", "A"} };
+        var rna = new StringBuilder();
+
+        for (var index = 0; index < nucleotide.Length; index++) {
+            var value = map[nucleotide[index].ToString()];
+            
+            rna.Append(value);
+        }
+        
+        return rna.ToString();
     }
 }
